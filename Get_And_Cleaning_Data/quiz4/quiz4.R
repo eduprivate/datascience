ae99b9c5804377ea22c98d0a3990de22534cc36c
Eduardo Ferreia <edu.private@gmail.com>
2014-08-26 22:28:17 -0300

    new code

--- Get_And_Cleaning_Data/quiz4/quiz4.R
+++ Get_And_Cleaning_Data/quiz4/quiz4.R
@@ -13,7 +13,8 @@ mean(numData)
 #q3
 countryNames <- dataGDP[5:194,4]
 class(countryNames)
-regexec("^United", strsplit(countryNames, ','))
+grep("^United",countryNames)
+grep("*United",countryNames)
 
 #q4
 newGdpData <- dataGDP[6:194, c(1, 2, 4, 5)]
@@ -26,3 +27,17 @@ head(mergedData)
 names(mergedData) <- tolower(names(mergedData))
 a <- as.character(mergedData[,13])
 length(grep("Fiscal year end: June", a))
+
+#q5
+install.packages("quantmod")
+library(quantmod)
+amzn = getSymbols("AMZN",auto.assign=FALSE)
+
+install.packages('lubridate')
+library('lubridate')
+sampleTimes <- index(amzn)
+length(sampleTimes)
+bool1 <- year(sampleTimes) == 2012
+length(sampleTimes[bool1]) # 250
+bool2 <- weekdays(sampleTimes)=="Monday"
+length(sampleTimes[bool1 & bool2]) # 47
